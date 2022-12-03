80c6f038e92c4ef593859bbc4b3f5c3a3834da33
vasanth12345 <vasanthvp1611@gmail.com>
2022-12-03 22:34:16 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [Data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("https://easy-gray-donkey-ring.cyclic.app/api/data/")
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((res) => {
+          setData(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>ha ha ha test rest</h1>
+      <div className="status">
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "10px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {typeof Data !== undefined && Data !== "" ? (
+            Data.map((e) => (
+              <div
+                key={e.name}
+                style={{
+                  border: "2px solid #f00",
+                  width: "30%",
+                  textAlign: "center",
+                }}
+              >
+                <h1>{e.name}</h1>
+                <h1>{e.age}</h1>
+                <p>{e.desig}</p>
+              </div>
+            ))
+          ) : (
+            <h1>noo data</h1>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
